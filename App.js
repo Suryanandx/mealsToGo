5c1091fe3c510ed4f62a3f14318ab70c27549ab1
Apple <apple@Apples-MacBook-Pro.local>
2022-07-31 16:08:37 +0530

    Restaurant services and mocks with context, Promises

--- App.js
+++ App.js
@@ -4,6 +4,34 @@ import { RestaurantScreen } from './src/features/restaurants/screens/restaurants
 import { theme } from './src/infrastructure/theme';
 import { useFonts as useOswald, Oswald_400Regular } from '@expo-google-fonts/oswald';
 import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Text } from 'react-native-paper';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { restaurantsRequest } from './src/services/restaurants/mock/restaurants.service';
+
+const TAB_ICON = {
+    Restaurants: "restaurant",
+    Map: "ios-map",
+    Settings: 'ios-settings'
+}
+
+const Settings = () => {
+  return  (
+    <Text>Settings here</Text>
+  )
+}
+
+const Maps = () => {
+  return  (
+    <Text>Maps here</Text>
+  )
+}
+
+restaurantsRequest()
+
+const Tab = createBottomTabNavigator();
+
 export default function App() {
 
   const [oswaldLoaded] = useOswald({
@@ -18,12 +46,32 @@ export default function App() {
     return null;
   }
 
- 
+const tabBarIcon = (iconName) => ({size, color}) => {
+  return <Ionicons name={iconName} size={size} color={color} />;
+  }
 
+const screenOptions = ({route}) => {
+  const iconName = TAB_ICON[route.name]
+  return{
+    tabBarIcon : tabBarIcon(iconName),
+    tabBarActiveTintColor: 'tomato',
+    tabBarInactiveTintColor: 'gray',
+  }
+}
   return (
+    <>
     <ThemeProvider theme={theme}>
-      <RestaurantScreen/>
+      <NavigationContainer >
+        <Tab.Navigator
+          screenOptions={screenOptions} 
+        >
+          <Tab.Screen name="Restaurants" component={RestaurantScreen} />
+          <Tab.Screen name="Map" component={Maps} />
+          <Tab.Screen name="Settings" component={Settings} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </ThemeProvider>
-   
+
+  </>
   );
 }
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,45 +1,53 @@
-import { StatusBar as ExpoStatusBar} from 'expo-status-bar';
-import { StatusBar, StyleSheet, FlatList, View, SafeAreaView,} from 'react-native';
+
+import {  StyleSheet, FlatList} from 'react-native';
 import {Searchbar} from 'react-native-paper'
 import styled from 'styled-components/native';
 import { Spacer } from '../../../components/spacer.component';
+import { SafeArea } from '../../../components/utility/safe-area.component';
 import RestaurantInfoCard from '../components/restaurant-info-card.component';
 
-const SafeArea = styled(SafeAreaView)`
-    flex: 1;
-   ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px;`} 
-`;
 
 const SearchContainer = styled.View`
-    padding: ${(props) => props.theme.space[3]};
+  padding: ${(props) => props.theme.space[3]};
 `;
 
-const RestaurantList = styled(FlatList).attrs`
-padding: ${(props) => props.theme.space[3]};
-`
-export const RestaurantScreen = () => {
-    return (
-        <SafeArea>
-        <SearchContainer >
-          <Searchbar/>
-        </SearchContainer>
-        <FlatList
-          data={[1,2,3,4,5]}
-          renderItem={() => 
-          <>
-          <Spacer position="bottom"  size="large">
-            <RestaurantInfoCard/>
-          </Spacer>
-          
-          </>}
-          keyExtractor={(item) => item.name}
-          contentContainerStyle={{padding: 16}}
-        />
-         
-        <ExpoStatusBar/>
-        </SafeArea>
-    )
-}
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 16,
+  },
+})``;
+
+export const RestaurantScreen = () => (
+  <SafeArea>
+    <SearchContainer>
+      <Searchbar />
+    </SearchContainer>
+    <RestaurantList
+      data={[
+        { name: 1 },
+        { name: 2 },
+        { name: 3 },
+        { name: 4 },
+        { name: 5 },
+        { name: 6 },
+        { name: 7 },
+        { name: 8 },
+        { name: 9 },
+        { name: 10 },
+        { name: 11 },
+        { name: 12 },
+        { name: 13 },
+        { name: 14 },
+      ]}
+      renderItem={() => (
+        <Spacer position="bottom" size="large">
+          <RestaurantInfoCard />
+        </Spacer>
+      )}
+      keyExtractor={(item) => item.name}
+    />
+  </SafeArea>
+);
 
 
 const styles = StyleSheet.create({
